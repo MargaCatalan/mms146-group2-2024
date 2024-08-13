4c6ca6408c5fd149ac817a7a563933590c5003cb
bea_feliciano <152283428+sbeafeliciano@users.noreply.github.com>
2024-08-13 18:16:24 +0800

    Update student.py

--- student.py
+++ student.py
@@ -1,2 +1,10 @@
 class Student:
-    pass
+    def __init__(self, name, answers_given):
+    self.name = name
+    self.answers_given = answers_given
+
+    def save_answer(self):
+        pass
+
+    def get_performance (self):
+        pass
