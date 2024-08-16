c21f7f95e042812627f79815bca74f9e0ab1389c
bea_feliciano <152283428+sbeafeliciano@users.noreply.github.com>
2024-08-16 18:46:55 +0800

    Update student.py

--- student.py
+++ student.py
@@ -1,10 +1,40 @@
 class Student:
-    def __init__(self, name, answers_given):
-    self.name = name
-    self.answers_given = answers_given
+    def __init__(self, name):
+        self.name = name
+        # List of dictionaries to store the answers given by the student for each subject
+        self.english_answers_given = []
+        self.filipino_answers_given = []
+        self.math_answers_given = []
+        self.art_answers_given = []
+        self.science_answers_given = []
+        
+ def save_answer(self, subject, question, options, given_answer, correct_answer):
+        if subject == "english":
+            self.english_answers_given.append({'question' : question,
+                                               'options' : options,
+                                               'given_answer' : given_answer,
+                                               'correct_answer' : correct_answer})
+        elif subject == "filipino":
+            self.filipino_answers_given.append({'question' : question,
+                                               'options' : options,
+                                               'given_answer' : given_answer,
+                                               'correct_answer' : correct_answer})
+        elif subject == "math":
+            self.math_answers_given.append({'question' : question,
+                                               'options' : options,
+                                               'given_answer' : given_answer,
+                                               'correct_answer' : correct_answer})
+        elif subject == "art":
+            self.art_answers_given.append({'question' : question,
+                                               'options' : options,
+                                               'given_answer' : given_answer,
+                                               'correct_answer' : correct_answer})
+        elif subject == "science":
+            self.science_answers_given.append({'question' : question,
+                                               'options' : options,
+                                               'given_answer' : given_answer,
+                                               'correct_answer' : correct_answer})
 
-    def save_answer(self):
-        self.answers_given.append()
-
-    def get_performance (self):
-        pass
+        def get_performance_report(self):
+            pass
+            
