e2a50bd073f935a5e230ce9c7699b5a53a7cfea8
Glyza Catalan <117784708+glyzacatalan@users.noreply.github.com>
2024-08-22 18:38:37 +0800

    Update student.py

--- student.py
+++ student.py
@@ -1,10 +1,1 @@
-class Student:
-    def __init__ (self, name, answers_given):
-        self.name = name 
-        self.answers_given = answers_given
 
-    def save_answer(self):
-        pass
-
-    def get_performance_report(self):
-        pass
