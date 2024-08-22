2a5c32f5a0f501595f7c232d683f24f6941850fc
Glyza Catalan <117784708+glyzacatalan@users.noreply.github.com>
2024-08-22 20:17:50 +0800

    Update student.py
    Allocating spaces for individual task inputs of members based on task codes

--- student.py
+++ student.py
@@ -1,1 +1,21 @@
+# Task Code: Student_1
+# Insert your work/contributions below
 
+
+
+
+
+
+
+# Task Code: Student_1
+# Insert your work/contributions below
+
+
+
+
+
+
+
+
+# Task Code: Student_1
+# Insert your work/contributions below
