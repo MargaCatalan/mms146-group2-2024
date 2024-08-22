a2bf6bdbc06b28349f7ee06c35743b29233a71a7
Glyza Catalan <117784708+glyzacatalan@users.noreply.github.com>
2024-08-22 20:14:30 +0800

    Update examreviewer.py
    Allocating spaces for individual task inputs of members based on task codes

--- examreviewer.py
+++ examreviewer.py
@@ -1,1 +1,39 @@
+# Task Code: Exam_Reviewer_1
+# Insert your work/contributions below
+
+
+
+
+
+
+
+# Task Code: Exam_Reviewer_2
+# Insert your work/contributions below
+
+
+
+
+
+
+
+
+# Task Code: Exam_Reviewer_3
+# Insert your work/contributions below
+
+
+
+
+
+
+
+
+# Task Code: Exam_Reviewer_4
+# Insert your work/contributions below
+
+
+
+
+
+
+
 
