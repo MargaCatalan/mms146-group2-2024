d46913d753a5d18ea0feb7d5c9312c94aa030785
Glyza Catalan <117784708+glyzacatalan@users.noreply.github.com>
2024-08-22 20:18:53 +0800

    Update menu.py
    Allocating spaces for individual task inputs of members based on task codes

--- menu.py
+++ menu.py
@@ -1,1 +1,42 @@
+# Task Code: Menu_1
+# Insert your work/contributions below
 
+
+
+
+
+
+
+# Task Code: Menu_2
+# Insert your work/contributions below
+
+
+
+
+
+
+
+
+# Task Code: Menu_3
+# Insert your work/contributions below
+
+
+
+
+
+
+
+
+# Task Code: Menu_4
+# Insert your work/contributions below
+
+
+
+
+
+
+
+
+
+# Task Code: Menu_5
+# Insert your work/contributions below
