0a55f09222c74bae12227b1f434c045ff2f4a957
Glyza Catalan <117784708+glyzacatalan@users.noreply.github.com>
2024-08-25 22:21:39 +0800

    Update menu.py
    Inserted Menu_1, Menu_2, and Menu_3 task codes

--- menu.py
+++ menu.py
@@ -1,5 +1,49 @@
 # Task Code: Menu_1
-# Insert your work/contributions below
+"Menus"
+def Main_Menu():                      
+    while True:
+        print("\n\n----------------------  Group 2’s Grade 10 Exam Reviewer  ---------------------\n\n")
+        print("Hi, {}!".format(user_name.title()))
+        print("What do you wish to do?")
+        print("  [1] Review a Subject")
+        print("  [2] View My Previous Exam Performance Reports")
+        print("  [0] None")
+
+        try:
+            choice = int(input('\n'"Please choose an option: "))
+            
+            if choice == 1:
+                Review_Menu()
+                  
+            elif choice == 2:                                                 
+                View_Report_Menu()
+                  
+            elif choice == 0:
+                Exit_Menu()
+
+            else:
+                print("Invalid input. Please try again.\n")  
+        
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+def Review_Menu():
+    print('\n''\n'"-------------------------------  Hello, {}!  -------------------------------"'\n''\n'.format(user_name.title()))
+    while True:
+        print("Which subject do you wish to review?")
+        print("  [1] English")
+        print("  [2] Math")
+        print("  [3] Filipino")
+        print("  [4] Science")
+        print("  [5] Art")
+        print("  [6] Any (Random)")
+        print("  [0] Back")
+
+        try:
+            choice = int(input('\n'"Please choose an option: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.\n")
+            continue
 
 
 
@@ -18,7 +62,23 @@
 
 
 # Task Code: Menu_3
-# Insert your work/contributions below
+def View_Report_Menu():
+    while True:
+        print('\n''\n'"----------------------  Viewing Previous Exam Performance Reports  ---------------------"'\n''\n'.format(user_name.title()))
+        print("Which subject’s exam performance report do you wish to view?")
+        print("  [1] English")
+        print("  [2] Math")
+        print("  [3] Filipino")
+        print("  [4] Science")
+        print("  [5] Art")
+        print("  [6] All")
+        print("  [0] Back")
+
+        try:
+            choice = int(input('\nPlease choose an option: '))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
 
 
 
@@ -39,4 +99,36 @@
 
 
 # Task Code: Menu_5
-# Insert your work/contributions below
+def Continue_Menu():
+    while True:
+        choice = input('\n'"Do you wish to have another session in the program? (Yes/No): ").lower()
+    
+        if choice == "yes":
+            Main_Menu()
+
+        elif choice == "no":
+            Exit_Menu()
+
+        else:
+            print("Invalid input. Please try again.")
+
+def Exit_Menu():
+    while True:
+        choice_2 = input('\n'"Do you wish to exit the program? (Yes/No): ").lower()
+
+        if choice_2 == "yes":
+            print("Thank you for using Group 2’s Grade 10 Exam Reviewer, {}.".format(user_name.capitalize()))
+            print("The program will close now.\n")
+            exit()
+
+        elif choice_2 == "no":
+            break
+        
+        else:
+            print("Invalid input. Please try again.")
+
+"Running the program" 
+while True:
+    print('\n''\n'"----------------------  Welcome to Group 2’s Grade 10 Exam Reviewer  ---------------------"'\n''\n')
+    user_name = input('\n'"Please enter your name: ")
+    Main_Menu()
