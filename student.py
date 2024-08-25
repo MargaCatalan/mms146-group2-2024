7cdc0aaf8a87370357d5de44085a2a4d7ae45c3e
Nina <gaguiang@up.edu.ph>
2024-08-25 13:32:30 +0800

    Update student.py

--- student.py
+++ student.py
@@ -1,12 +1,14 @@
 # Task Code: Student_1
 # Insert your work/contributions below
-
-
-
-
-
-
-
+class Student:
+    def __init__(self, name):
+        self.name = name
+        # List of dictionaries to store the answers given by the student for each subject
+        self.english_answers_given = [correct_answer] # type: ignore
+        self.filipino_answers_given = [correct_answer] # type: ignore
+        self.math_answers_given = [correct_answer] # type: ignore
+        self.art_answers_given = [correct_answer] # type: ignore
+        self.science_answers_given = [correct_answer] # type: ignore
 # Task Code: Student_1
 # Insert your work/contributions below
 
