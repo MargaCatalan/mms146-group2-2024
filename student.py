a3ba13e7c799877663d8bb122a0aea59375be0c1
bea_feliciano <152283428+sbeafeliciano@users.noreply.github.com>
2024-08-25 14:33:05 +0800

    Update student.py

--- student.py
+++ student.py
@@ -8,8 +8,33 @@ class Student:
         self.art_answers_given = [correct_answer] # type: ignore
         self.science_answers_given = [correct_answer] # type: ignore
 
-# Task Code: Student_1
-# Insert your work/contributions below
+# Task Code: Student_2
+    def save_answer(self, subject, question, options, answer, correct_answer):
+        if subject == "english":
+            self.english_answers_given.append({'question' : question,
+                                               'options' : options,
+                                               'given_answer' : answer,
+                                               'correct_answer' : correct_answer})
+        elif subject == "filipino":
+            self.filipino_answers_given.append({'question' : question,
+                                               'options' : options,
+                                               'given_answer' : answer,
+                                               'correct_answer' : correct_answer})
+        elif subject == "math":
+            self.math_answers_given.append({'question' : question,
+                                               'options' : options,
+                                               'given_answer' : answer,
+                                               'correct_answer' : correct_answer})
+        elif subject == "art":
+            self.art_answers_given.append({'question' : question,
+                                               'options' : options,
+                                               'given_answer' : answer,
+                                               'correct_answer' : correct_answer})
+        elif subject == "science":
+            self.science_answers_given.append({'question' : question,
+                                               'options' : options,
+                                               'given_answer' : answer,
+                                               'correct_answer' : correct_answer})
 
 
 
